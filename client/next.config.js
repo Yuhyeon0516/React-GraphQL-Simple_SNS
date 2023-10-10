50b8ac5b0372d5975fc450f1aeb2f615a0af26e9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-10 21:26:02 +0900

    Project reset

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
 const path = require('path');
 
-const nextConfig = {
+module.exports = {
     sassOptions: {
-        includePaths: [path.join(__dirname), 'styles'],
+        includePaths: [path.join(__dirname, 'styles')],
     },
 };
-
-module.exports = nextConfig;
