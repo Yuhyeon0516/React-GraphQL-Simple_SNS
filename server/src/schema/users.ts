d959e7f65eba8724eb2a25373021657f774dfa62
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-11 15:32:23 +0900

    Defined db schema

---
+++ server/src/schema/users.ts
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server-express';
+
+const usersSchema = gql`
+    type User {
+        id: ID!
+        nickname: String!
+    }
+
+    extend type Query {
+        users(): [User!]!
+        user(id: ID!): User
+    }
+`;
+
+export default usersSchema;
