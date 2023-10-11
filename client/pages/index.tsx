d7628bf164172859644aac005464b6b9f41458f8
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-11 14:03:05 +0900

    Applied SSR using getServerSideProps function.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import MsgList from '../components/MsgList';
+import fetcher from '../utils/fetcher';
 
-export default function Home() {
+export default function Home({ smsgs }) {
     return (
         <div>
             <h1>Simple SNS</h1>
-            <MsgList />
+            <MsgList smsgs={smsgs} />
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const { data: smsgs } = await fetcher.get('/messages');
+    return {
+        props: { smsgs },
+    };
 }
